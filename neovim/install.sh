29a92019df07c77fa15cdb311abd87fec41d8c00
Djoe Pramono <djoepramono@gmail.com>
2017-06-20 23:42:32 +1000

    Copy the config straight to nvim init.vim instead of .vimrc

--- neovim/install.sh
+++ neovim/install.sh
@@ -9,7 +9,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Copy .vimrc from repository
-cp .vimrc ~/
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+cp .vimrc ~/.config/nvim/init.vim
 
 # Done, install the plugins from inside neovim with :PlugInstall
